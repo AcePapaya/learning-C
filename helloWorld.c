88a1dd44e5aeede80f2043591c71292eb8e6e627
AcePapaya <papsithepepsi@gmail.com>
2026-05-12 12:39:04 +1200

    Beginning of str input

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 
 
 float addNum(float num1, float num2){
@@ -32,9 +35,27 @@ void printStr(char *msg, size_t lengthMsg){
     }
 }
 
-double getNum(char *msg, size_t lengthMsg, bool forceInt){
-
+double getDbl(char *msg, size_t lengthMsg){
     double num;
+    char inp[100];
+
+    printStr(msg, lengthMsg);
+    printf("\n");
+
+    while (fgets(inp, sizeof(inp), stdin)) {
+            // Try to read an integer from the input string
+             if (sscanf(inp, "%lf", &num) == 1) {
+            break; // Success: break out of the loop
+            } else {
+            printf("Invalid input. Try again: "); // If not an integer, ask again
+        }
+    }
+    return num;
+}
+
+
+int getInt(char *msg, size_t lengthMsg){
+    int num;
     char inp[100];
 
     printStr(msg, lengthMsg);
@@ -47,16 +68,6 @@ double getNum(char *msg, size_t lengthMsg, bool forceInt){
             } else {
             printf("Invalid input. Try again: "); // If not an integer, ask again
         }
-        // if (forceInt){
-           
-        // }
-        
-        // else {
-        //     if (sscanf(inp, "%lf", &num) == 1) {
-        //     break; // Success: break out of the loop
-        //     } else {
-        //     printf("Invalid input. Try again: "); // If not an integer, ask again
-        // }}
     }
     return num;
 }
@@ -66,35 +77,65 @@ int chooseOp(){
     printStr(msg, strlen(msg));
     char *options[2] = {
         "1. Add\n",
-        "2. Subtract\n"
+        "2. Subtract"
     };
     int n = sizeof(options)/sizeof(options[0]);
     for (int i = 0; i < n; i++){
         printStr(options[i], strlen(options[i]));
     }
 
-    int choice = getNum("", strlen(""), 1);
+    int choice = getInt("", strlen(""));
     return choice;
 }
 
+// bool isOp(char c){
+//     return(c >= 40 && c <= 47);
+// }
+// struct brokenDown{
+//         float num1;
+//         char op;
+//         float num2;
+//     };
+
+//  struct brokenDown strToFormattedStruct(char *str, int strLen){
+//     float *nums[2];
+//     const int MULTIPLIER = 10;
+//     char op;
+
+//     struct brokenDown a;
+
+    // for (int i = 0; i < strLen; i++){
+    //     if(isnum(str[i])){
+    //         *nums = nums;
+    //     }
+    // }
+// }
+
 int main() {
-    int num1, num2;
+    double num1, num2;
     double ans;
+
+    // char test[] = "50+10";
+
+    // strToFormattedStruct(test, strlen(test));
+
+
 
     printf("Welcome to my C calculator!\n");
 
 
     char msg1[] = "Please enter the first number:";
-    num1 = getNum(msg1, strlen(msg1), 0);
+    num1 = getDbl(msg1, strlen(msg1));
 
     char msg2[] = "Please enter the second number:";
-    num2 = getNum(msg2, strlen(msg2), 0);
+    num2 = getDbl(msg2, strlen(msg2));
 
     
     bool endLoop =  0;
 
     do{
     int choice = chooseOp();
+    // printf("%d", choice);
 
     switch (choice) {
         case 1:
@@ -107,15 +148,15 @@ int main() {
             endLoop = 1; 
             break;
         default:
-            char msg[] = "Please enter a valid number";
+            char msg[] = "Please enter a valid number\n";
             printStr(msg, strlen(msg));
             break;
-    }
+        }
     } while (!endLoop);
 
     ans = truncFloat(ans);
 
-    printf("The answer is: %.1f", ans);
+    printf("The answer is: %f", ans);
 
     return 0;
 }
